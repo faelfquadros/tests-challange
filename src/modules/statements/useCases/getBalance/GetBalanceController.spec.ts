c62cfa173c737fb9b252abd313c90811fbd78f65
rafaelfquadros <rafael.rffq@gmail.com>
2022-06-27 21:51:05 -0300

    feat: :sparkles: Get user balance supertest

--- src/modules/statements/useCases/getBalance/GetBalanceController.spec.ts
+++ src/modules/statements/useCases/getBalance/GetBalanceController.spec.ts
@@ -7,10 +7,9 @@ import { app } from '../../../../app';
 
 let connection: Connection;
 let password = 'admin';
-const _baseApi = '/api/v1/';
-const fakeToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiODFlMzliOGEtOTgxYS00MzNkLWExYTUtMWQ4NGE4OTU5OWUzIiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkB0ZXN0c2NoYWxsYW5nZS5jb20iLCJwYXNzd29yZCI6IiQyYSQwOCRzSUx1Nm5HNTJncEdUelV1cGJRbmxld1pkdkpXbTNINWdxbE1kYlRpV3ltWTF5Lnh6UXlaaSIsImNyZWF0ZWRfYXQiOiIyMDIyLTA2LTI2VDIzOjI0OjQzLjE2NFoiLCJ1cGRhdGVkX2F0IjoiMjAyMi0wNi0yNlQyMzoyNDo0My4xNjRaIn0sImlhdCI6MTY1NjI3NTA4MywiZXhwIjoxNjU2MzYxNDgzLCJzdWIiOiI4MWUzOWI4YS05ODFhLTQzM2QtYTFhNS0xZDg0YTg5NTk5ZTMifQ.F_vdZg6msHCTjmVXGG1EjfrP3zwU43V5z-bOJRnOm8c`;
+const _baseApi = '/api/v1/statements/';
 
-describe('Show user profile controller', () => {
+describe("Get ballance controlle", () => {
     beforeAll(async () => {
         connection = await createConnection();
         await connection.runMigrations();
@@ -28,9 +27,9 @@ describe('Show user profile controller', () => {
         await connection.close();
     });
 
-    it('Should be able to get logged user profile', async () => {
+    it("Should be able to get balance from logged user", async () => {
         const authenticateResponse = await request(app)
-            .post(`${_baseApi}sessions`)
+            .post(`/api/v1/sessions`)
             .send({
                 email: "admin@testschallange.com",
                 password,
@@ -38,12 +37,23 @@ describe('Show user profile controller', () => {
 
         const { token } = authenticateResponse.body;
 
-        const showProfileResponse = await request(app)
-            .get(`${_baseApi}profile`)
+        const deposit = await request(app)
+            .post(`${_baseApi}deposit`)
+            .send({
+                amount: 100.50, 
+                description: "first deposit"
+            })
             .set({
                 Authorization: `Bearer ${token}`,
             });
-        
-        expect(showProfileResponse.status).toBe(200);
+
+        const getUserBalance = await request(app)
+            .get(`${_baseApi}balance`)
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(getUserBalance.status).toBe(200);
+        expect(getUserBalance.body).toHaveProperty("statement");
     });
 })
